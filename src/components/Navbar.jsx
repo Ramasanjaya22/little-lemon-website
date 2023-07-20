dccb719c88ab214a2334d7cea6bc197b8d22ae08
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-07-21 00:41:52 +0700

    relocate image & navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import navLogo from "../assets/nav-logo.png";
+import Navigation from "./Navigation";
+import Hamburger from "../assets/hamburger.png";
+import Close from "../assets/close.png";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  function handleToggle() {
+    setNavbarOpen(!navbarOpen);
+  }
+  return (
+    <nav>
+      <nav className="burger">
+        <img src={navLogo} alt="Little Lemon logo" className="nav-image"></img>
+        <button className="burger-icon" onClick={handleToggle}>
+          <img src={navbarOpen ? Close : Hamburger} alt="Navigation Bar" />
+        </button>
+      </nav>
+      <Navigation device="desktop" />
+      {navbarOpen ? <Navigation device="mobile" /> : ""}
+    </nav>
+  );
 };
 
 export default Navbar;
