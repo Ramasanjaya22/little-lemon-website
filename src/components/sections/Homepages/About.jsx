fbba01cc92bdaddc2616ae95d1233adb56b77134
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-07-22 23:13:25 +0700

    add about

--- src/components/sections/Homepages/About.jsx
+++ src/components/sections/Homepages/About.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <article className="about-us">
+      <section className="hero-text">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p className="about-subtext">
+          Little Lemon was established in 1995 by Adrian and Mario, two Italian
+          brothers. Despite the diverse culinary scene in Chicago, they noticed
+          the absence of Mediterranean cuisine and were motivated to introduce
+          the authentic flavors of their hometown in Italy to the city. Even
+          after almost thirty years, the two brothers remain actively involved
+          in managing and overseeing the operations of Little Lemon restaurant.
+        </p>
+      </section>
 
-export default About
+      <section className="double-image">
+        <img
+          className="about-1"
+          src={require("../../../assets/food/food3.jpeg")}
+          alt="Little Lemon restaurant cuisine 1"
+        ></img>
+        <img
+          className="about-2"
+          src={require("../../../assets/food/food2.jpeg")}
+          alt="Little Lemon restaurant cuisine 2"
+        ></img>
+      </section>
+    </article>
+  );
+};
+
+export default About;
